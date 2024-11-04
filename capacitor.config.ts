b6ec01cf1756ebc0e183083a13b8bec7da5ea56c
Marlon Harrison <marlon@ionic.io>
2024-11-04 15:10:07 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'demo-ac6-salesforce',
+  webDir: 'www'
+};
+
+export default config;
