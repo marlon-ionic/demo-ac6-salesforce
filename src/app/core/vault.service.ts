52a6a4ab3d460b02c68afd36599c518213355cc8
Marlon Harrison <marlon@ionic.io>
2024-11-04 16:15:58 -0600

    write vaultService

--- src/app/core/vault.service.ts
+++ src/app/core/vault.service.ts
@@ -1,9 +1,64 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, NgZone } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { BiometricPermissionState, BrowserVault, Device, DeviceSecurityType, IdentityVaultConfig, Vault, VaultError, VaultType } from '@ionic-enterprise/identity-vault';
+import { ReplaySubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VaultService {
+  private readonly zone = inject(NgZone);
+  private readonly vault: BrowserVault | Vault;
+  private readonly vaultErrorSubject = new Subject<VaultError>();
+  private readonly vaulLockSubject = new ReplaySubject<VaultLockState>(1);
 
-  constructor() { }
+  constructor() {
+    this.vault = Capacitor.isNativePlatform() ? new Vault() : new BrowserVault();
+  }
+
+  vaultError$ = this.vaultErrorSubject.asObservable();
+  vaultLock$ = this.vaulLockSubject.asObservable();
+
+  async init() {
+    const biometricsSupported = await Device.isBiometricsSupported();
+    const vaultConfig: IdentityVaultConfig = {
+      type: VaultType.InMemory,
+      key: 'io.ionic.demo-ac6-vault',
+      deviceSecurityType: DeviceSecurityType.None,
+      shouldClearVaultAfterTooManyFailedAttempts: true
+    };
+    if(biometricsSupported) {
+      const permissions = await Device.isBiometricsAllowed();
+      if (permissions === BiometricPermissionState.Denied) {
+        // Prompt user that Biometrics are disabled?
+        } else {
+          vaultConfig.deviceSecurityType = DeviceSecurityType.Both;
+          vaultConfig.type = VaultType.DeviceSecurity;
+        }
+    }
+    this.vault.onLock((lockEvent) => {
+      this.zone.run(() => {
+        this.vaulLockSubject.next({ isLocked: true, timeout: lockEvent.timeout });
+      });
+    });
+    this.vault.onUnlock(() => {
+      this.zone.run(() => {
+        this.vaulLockSubject.next({ isLocked: false });
+      });
+    });
+    this.vault.onError((error) => {
+      this.zone.run(() => {
+        this.vaultErrorSubject.next(error);
+      });
+    });
+    await this.vault.initialize(vaultConfig);
+
+    const isLocked = await this.vault.isLocked();
+    this.vaulLockSubject.next({ isLocked });
+  }
+}
+
+export interface VaultLockState {
+  isLocked: boolean;
+  timeout?: boolean;
 }
