e48e352533f8905334061e1bd22c8c171d30bbb0
Marlon Harrison <marlon@ionic.io>
2024-11-04 18:59:08 -0600

    fix redirectUri

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,10 +8,10 @@ import { ProviderOptions } from "@ionic-enterprise/auth";
 const providerOptions: ProviderOptions = {
   audience: '',
   clientId: '3MVG91oqviqJKoEGK1XLbMkrVmBbc3G8Y3pY0sf4IBo3jimUuUdRnMJ.X3ELckx3IQZwOnQfS9S2_K8OFd7kk',
-  redirectUri: Capacitor.isNativePlatform() ? 'msauth:/auth-action-complete' : 'http://localhost:8100/auth-action-complete',
+  redirectUri: Capacitor.isNativePlatform() ? 'msauth://auth-action-complete' : 'http://localhost:8100/auth-action-complete',
   discoveryUrl: 'https://outsystems8-dev-ed.develop.my.salesforce.com/.well-known/openid-configuration',
   scope: 'openid profile email offline_access',
-  logoutUrl: Capacitor.isNativePlatform() ? 'msauth:/auth-action-complete' : 'http://localhost:8100/home'
+  logoutUrl: Capacitor.isNativePlatform() ? 'msauth://auth-action-complete' : 'http://localhost:8100/home'
 };
 
 export const environment = {
